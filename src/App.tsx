92935e110cc2b428edd3440ec6488c501443ae38
Jack Donaldson <jackdonaldson005@gmail.com>
2022-11-07 18:07:24 +1100

    Add share metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
 import { useMutation } from 'react-query'
 import './App.css'
